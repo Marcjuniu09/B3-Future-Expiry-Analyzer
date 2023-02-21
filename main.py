f33538e2855b20a1675c0c5c99a1069113ceffb6
Marcelo Junio Carvalho de Espindola <100105640+Marcjuniu09@users.noreply.github.com>
2023-02-21 01:34:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,24 +4,18 @@ import expiracao_de_contratos_B3 as exp
 df = pd.read_excel('symbol.xlsx')
 df = pd.read_excel('symbol.xlsx', sheet_name='Sheet1', header=0, index_col=0)
 
-for i, row in df.iterrows():
-    if i[:3] == 'WDO':
-        df.at[i, 'expiration'] = exp.DOL_WDO_DI1(i)
-    elif i[:3] == 'DOL':
-        df.at[i, 'expiration'] = exp.DOL_WDO_DI1(i)
-    elif i[:3] == 'DI1':
-        df.at[i, 'expiration'] = exp.DOL_WDO_DI1(i)
-    elif i[:3] == 'WIN':
-        df.at[i, 'expiration'] = exp.expiration_WIN(i)
-    elif i[:3] == 'IND':
-        df.at[i, 'expiration'] = exp.expiration_IND(i)
-    elif i[:3] == 'BGI':
-        df.at[i, 'expiration'] = exp.expiration_BGI_ETN(i)
-    elif i[:3] == 'ETN':
-        df.at[i, 'expiration'] = exp.expiration_BGI_ETN(i)
-    elif i[:3] == 'SFI':
-        df.at[i, 'expiration'] = exp.expiration_SFI(i)
-    elif i[:3] == 'CCM':
-        df.at[i, 'expiration'] = exp.expiration_CCM(i)
-    elif i[:3] == 'ICF':
-        df.at[i, 'expiration'] = exp.expiration_ICF(i)
+expiration_functions = {'WDO': exp.DOL_WDO_DI1,
+                        'DOL': exp.DOL_WDO_DI1,
+                        'DI1': exp.DOL_WDO_DI1,
+                        'WIN': exp.expiration_WIN,
+                        'IND': exp.expiration_IND,
+                        'BGI': exp.expiration_BGI_ETN,
+                        'ETN': exp.expiration_BGI_ETN,
+                        'SFI': exp.expiration_SFI,
+                        'CCM': exp.expiration_CCM,
+                        'ICF': exp.expiration_ICF}
+
+for index, row in df.iterrows():
+    prefix = index[:3]
+    if prefix in expiration_functions:
+        df.at[index, 'expiration'] = expiration_functions[prefix](index)
